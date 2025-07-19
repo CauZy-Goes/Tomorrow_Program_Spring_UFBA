eb8948909d70a54470b852730722a483c68dcca7
CauZy-Goes <cauafariasdev@gmail.com>
2025-07-19 11:19:21 -0300

    feat: add ola endpoint

---
+++ tomorrowprogram/src/main/java/br/ufba/tomorrow/tomorrowprogram/tomorrowprogram/AloController.java
@@ -0,0 +1,18 @@
+package br.ufba.tomorrow.tomorrowprogram.tomorrowprogram;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController()
+@RequestMapping("/ola")
+public class AloController {
+    @GetMapping()
+    public String alo() {
+        return "Alô, mundo!";
+    }
+
+    @GetMapping("/dizer/{quem}")
+    public String ola(@PathVariable  String quem){
+        return "Ola, " + quem;
+    }
+}
+
--- tomorrowprogram/src/main/java/br/ufba/tomorrow/tomorrowprogram/tomorrowprogram/usuario.java
+++
@@ -1,9 +0,0 @@
-package br.ufba.tomorrow.tomorrowprogram.tomorrowprogram;
-
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-@Data
-@RequiredArgsConstructor
-public class usuario {
-}
