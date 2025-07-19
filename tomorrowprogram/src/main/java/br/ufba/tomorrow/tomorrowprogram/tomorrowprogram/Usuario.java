b174f61ee2154052318c2da8f4a2afbd91022ef7
CauZy-Goes <cauafariasdev@gmail.com>
2025-07-19 13:31:43 -0300

    feat: add usuario entity

---
+++ tomorrowprogram/src/main/java/br/ufba/tomorrow/tomorrowprogram/tomorrowprogram/Usuario.java
@@ -0,0 +1,13 @@
+package br.ufba.tomorrow.tomorrowprogram.tomorrowprogram;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Usuario {
+    private  String nome;
+    private  String email;
+}
